5ceb86b7de636a83184690291fac66f62d777bed
@yordi68 <yordanos.yirgu1@gmail.com>
2023-08-01 16:19:22 +0300

    install lodash again and add index

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 let lodash = require("lodash");
-let arr = [2, 1, 8, 4, 5, 8];
+let array = [2, 1, 8, 4, 5, 8];
 
-console.log("Before sorting: ", arr);
-console.log("After sorting: ", lodash.sortBy(arr));
+console.log("Before sorting: ", array);
+console.log("After sorting: ", lodash.sortBy(array));
+
+let arr = [
+  {val:10, size:100},
+  {val:9, size:150},
+  {val:11, size:10},
+  {val:1, size:1000},
+  {val:74, size:140},
+  {val:7, size:100},
+];
+  
+console.log("sorted by val: \n", 
+  lodash.sortBy(arr, (e) => {
+    return e.val
+}));
+  
+console.log("sorted by size: \n", 
+  lodash.sortBy(arr, (e) => {
+    return e.size
+}));
+
